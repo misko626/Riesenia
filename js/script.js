48d1ea5e24a715fe37237bdb2e2ad4df3110be0f
MichalPopovic <popovic.michal626@gmail.com>
2022-10-05 23:31:26 +0200

    animations

--- js/script.js
+++ js/script.js
@@ -8,3 +8,37 @@ closeForm.addEventListener("click", function () {
 function openForm() {
   formContainer.classList.add("active");
 }
+
+// appearEffect
+
+window.addEventListener("scroll", appearElement);
+
+function appearElement() {
+  //screen sroll value
+  const screenPosition = (window.innerHeight / 2) * 1.3;
+
+  //progress apear
+  const cards = document.querySelectorAll(".card");
+
+  for (let card of cards) {
+    const cardPosition = card.getBoundingClientRect().top;
+    if (cardPosition < screenPosition) {
+      card.classList.add("appear-element");
+    }
+  }
+
+  //path appar
+  const pathIthems = document.querySelectorAll(".path-item");
+  for (let pathIthem of pathIthems) {
+    const pathItemPosition = pathIthem.getBoundingClientRect().top;
+    if (pathItemPosition < screenPosition) {
+      pathIthem.classList.add("appear-element");
+    }
+  }
+
+  const focus = document.querySelector(".focus");
+  const focusPosition = focus.getBoundingClientRect().top;
+  if (focusPosition < screenPosition) {
+    focus.classList.add("appear-element");
+  }
+}
